ebfa030ce47dfec319a9a8b547f0035c825754e7
iMaxxxxxxx <141819436+iMaxxxxxxx@users.noreply.github.com>
2024-07-19 21:03:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2154e7f8-514f-40d2-b43c-f3145a252566';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
